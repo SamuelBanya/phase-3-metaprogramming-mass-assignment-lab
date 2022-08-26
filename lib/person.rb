722da4c73d89db8bfe7bc57d1d2bd912cf5fec3b
Samuel Banya <sbanya@fastmail.com>
2022-08-26 19:51:34 -0400

    Adding latest changes to assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+puts "Test Cases:"
+puts "\nBob:"
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+bob = Person.new(bob_attributes)
+
+puts "\nbob: ", bob
+
+puts "\nSusan:"
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+susan = Person.new(susan_attributes)
+
+puts "\nsusan: ", susan
